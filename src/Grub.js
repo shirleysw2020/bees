4cee81934f946e404cbdd730ab1bc18970388d98
Shirley Wu <shirleyswwu@gmail.com>
2020-07-08 10:58:58 -0700

    pass all Grub tests

--- src/Grub.js
+++ src/Grub.js
@@ -7,21 +7,23 @@
 */
 var Grub = function(age, color, food) {
   this.age = 0;
-  this.color = pink
-  this.food = jelly;
+  this.color = 'pink'
+  this.food = 'jelly';
 };
+Grub.prototype.eat = function() {
 
+}
 // subclass: Bee
 var Bee = function() {
-  Grub.call(this, )
-  this.age = 0;
-  this.color = pink
-  this.food = jelly;
+  Grub.call(this, age, color, food)
+  this.age = 5;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Grub;
 
 
-Bee.prototype.eat = function() {
+// Bee.prototype.eat = function() {
 
-};
+// };
