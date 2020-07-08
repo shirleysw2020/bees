b53281111530219339621774b90c47e6926d8592
Shirley Wu <shirleyswwu@gmail.com>
2020-07-08 12:00:00 -0700

    pass all forager test but trying to fix illegal constructor error at instantiation

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,21 @@
-var ForagerBee = function() {
+/*.
+├── Grub
+│   └── Bee
+│       ├── HoneyMakerBee
+│       └── ForagerBee
+│          └── RetiredForagerBee
+*/
+var ForagerBee = function(age, food, color) {
+  Bee.call(this, color);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = constructor;
+
+ForagerBee.prototype.forage = function() {
+  this.treasureChest.push('treasure');
+}
